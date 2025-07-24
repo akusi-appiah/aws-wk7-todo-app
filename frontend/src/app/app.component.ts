f197c02843d02b956ecb374c145684c1bd31daf2
Akwasi Kusi-Appiah <akwasi.kusi-appiah@amalitech.com>
2025-07-24 17:59:21 +0000

    apply notification

--- frontend/src/app/app.component.ts
+++ frontend/src/app/app.component.ts
@@ -1,13 +1,11 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { NotificationsContainerComponent } from './pages/notifications-container/notifications-container.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet,NotificationsContainerComponent],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
 })
-export class AppComponent {
-  title = 'frontend';
-}
+export class AppComponent {}
