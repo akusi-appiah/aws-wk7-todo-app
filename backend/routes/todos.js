43fac771fdafa031b25b3b9ae579b99e9097c78e
Akwasi Kusi-Appiah <akwasi.kusi-appiah@amalitech.com>
2025-07-26 22:59:56 +0000

    chore testing routes

--- backend/routes/todos.js
+++ backend/routes/todos.js
@@ -4,11 +4,13 @@ const todoController = require('../controllers/todos.js');
 
 const router = express.Router();
 
-router.get('/', todoController.getAllTodos);
-router.get('/:id', todoController.getTodoById);
-router.post('/', todoController.createTodo);
-router.put('/:id', todoController.updateTodo);
-router.delete('/:id', todoController.deleteTodo);
-router.patch('/:id/status', todoController.toggleTodoStatus);
+// router.get('/', todoController.getAllTodos);
+// router.get('/:id', todoController.getTodoById);
+// router.post('/', todoController.createTodo);
+// router.put('/:id', todoController.updateTodo);
+// router.delete('/:id', todoController.deleteTodo);
+// router.patch('/:id/status', todoController.toggleTodoStatus);
+
+router.get('/', (req, res) => res.json({ message: 'Todos' }));
 
 module.exports = router;
